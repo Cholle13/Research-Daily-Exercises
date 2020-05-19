6934e434c49aef82026807ba531ecdfff1289904
cholle13 <chrisaholle@gmail.com>
2020-05-19 09:37:41 -0500

    Example commit

---
+++ Days/Day_2.md
@@ -0,0 +1,22 @@
+# Daily Coding Exercises
+
+###  Day 2 - File IO
+Now that you have reviewed some of the basics with printing and formatting we will move on
+to file input and output.  
+
+File IO is very important in research. Most of the scripts you will write are going to have
+at least one file that is used as input and at least one file for output.  
+
+Tips when naming files:
+- Stay consistent in you naming convention.
+- Use descriptive file names (input.csv vs FEHM_Model_Calibration_Data.csv).
+- When you have multiple files being output use a file number at the end of the name.
+Example: Moisture_Data_1.csv, Moisture_Data_2.csv etc.
+- Do not use spaces in your file names.
+- Include the extension (generally it will be a *.csv or *.txt extension)
+
+Now time for some code. The process will be the same as yesterday. Type out the code provided 
+and try to determine what the output will be. Do not copy and paste. There will be a few short
+challenges in the comments, this will help you think about what you've learned and apply it.
+Good luck!
+
--- Scripts/example.py
+++ Scripts/example.py
@@ -1,1 +1,31 @@
+#import pandas as pd
+import os
+
+# When working in Windows the filepath slashes are flipped
+# Example path in Windows. Note that there are two back slashes. This is because a backslash is the escape
+# character and is used to represent other test items (\t for tab, \n for new line etc.)
+windows_example_filepath = "Data\\test.csv"
+
+# Simple variables to find the file
+working_directory = "../Data/"
+file_name = "test.txt"
+
+# Creating a file object is the first step to reading or writing to a file
+input_file = open(working_directory + file_name)
+# What happens if we open a file that does not exist? Comment out the line after running.
+input_test_file = open("Data/testing.csv")
+
+# Now that we have a file object let's use it.
+# If you look at the file it is split into two parts. Let's look at the first part. This will show you how to
+# Read in a file line by line. Since their are only two lines we will read them individually. Please note that for most
+# scripts you will write that involve reading line by line you will use a loop to dynamically read each line.
+first_line = input_file.readline()
+# Wow that was a tough one
+
+print(first_line)
+
+
+
+
+
 
